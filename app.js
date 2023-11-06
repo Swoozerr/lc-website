46c2dc565afa7b67842bdd13a7f240654b02798d
L <123719357+Swoozerr@users.noreply.github.com>
2023-11-06 16:35:18 -0600

    added transition for navbar scrolling in and out, github logo, and project button functionality

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const projectButton = document.querySelector('.navbar__links');
+const projectSection = document.querySelector('.projects');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
+const headerHeight = document.querySelector('.navbar').offsetHeight; // Get the height of the header
+
+document.addEventListener('DOMContentLoaded', function() { /* make sure HTML finished loading */
+    /* for mobile menu */
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.toggle('active')
+    })
+
+    /* for project jump */
+    projectButton.addEventListener('click', function() {
+        projectSection.scrollIntoView({ behavior: 'smooth' })
+    })
+
+    /* scroll navbar out of view */
+    let lastScrollTop = 0
+
+    window.addEventListener('scroll', function() {
+        const currentScrollTop = window.scrollY
+
+        if (currentScrollTop > lastScrollTop) {
+            // scrolling down
+            document.querySelector('.navbar').classList.add('scroll-down')
+            document.querySelector('.navbar').classList.remove('scroll-up')
+        } 
+        else {
+            // scrolling up
+            document.querySelector('.navbar').classList.add('scroll-up')
+            document.querySelector('.navbar').classList.remove('scroll-down')
+        }
+
+        lastScrollTop = currentScrollTop;
+    })
 })
+
